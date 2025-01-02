52204a602a3ceec219fdb3abeaee6c5edb0e8292
kattakapilkumar <kapilkatta123@gmail.com>
2025-01-02 15:59:02 +0530

    initial changes

--- basics1.js
+++ basics1.js
@@ -1,4 +1,11 @@
 var flt1 = 2.230;
-console.log(Math.round(flt1));
+console.log(Math.round(flt1)); //2 
 var flt2 = 2.902;
-console.log(Math.round(flt2));
+console.log(Math.round(flt2)); //3
+
+console.log('hello'+3); // Type Coercion or also called as Type Conversion.
+console.log(3+3);
+console.log('$'+20.95+7.99);
+
+console.log('$'+(20.95+7.99)); // String Also Follow Order of Operations in javascript like how numbers follow order of Operations (..) , * , + , -
+
